453a6bd3069e5b2e511e8aa4521c3839176e846b
Catalin Tugui <katamaxx23@gmail.com>
2023-09-09 18:17:19 +0300

    working with css modules

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <p>homepage</p>
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
